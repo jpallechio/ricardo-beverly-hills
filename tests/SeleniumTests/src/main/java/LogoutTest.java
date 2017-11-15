299b82d982ed95f3bdee143af76b9ebbf4ef286b
jpallechio <defiance018@gmail.com>
2017-11-15 10:55:21 -0800

    added Logout test

--- tests/SeleniumTests/src/main/java/LogoutTest.java
+++ tests/SeleniumTests/src/main/java/LogoutTest.java
@@ -1,30 +1,26 @@
 import junit.framework.TestCase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
 
-public class LoginTest extends TestCase {
+public class LogoutTest extends TestCase {
     private WebDriver chromeDriver;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         chromeDriver = new ChromeDriver();
         chromeDriver.get("http://rbh.greenrivertech.net/login");
-    }
 
-    public void testEmailLoginACP(){
+        //need to login to test the logout
         chromeDriver.findElement(By.id("email")).sendKeys("acp@acp.com");
         chromeDriver.findElement(By.id("password")).sendKeys("acpacp");
         chromeDriver.findElement(By.className("btn-primary")).click();
-
-        assertEquals(chromeDriver.getCurrentUrl(), "http://rbh.greenrivertech.net/part-company-claim");
     }
 
-    public void testEmailLoginPC(){
-        chromeDriver.findElement(By.id("email")).sendKeys("pc@pc.com");
-        chromeDriver.findElement(By.id("password")).sendKeys("acpacp");
-        chromeDriver.findElement(By.className("btn-primary")).click();
+    public void testLogout(){
+        chromeDriver.findElement(By.className("dropdown-toggle")).click();
+        chromeDriver.findElement(By.xpath("//a[contains(.,'Logout')]")).click();
 
-        assertEquals(chromeDriver.getCurrentUrl(), "http://rbh.greenrivertech.net/part-company-claim");
+        assertEquals(chromeDriver.getCurrentUrl(), "http://rbh.greenrivertech.net/login");
     }
 }
